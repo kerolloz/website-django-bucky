0df4af70387fce816d33d3bca82f13710f9053e7
Kerolloz <kerolloz@yahoo.com>
2018-07-29 06:13:57 +0200

    tut 36

--- music/urls.py
+++ music/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     # /music/
     url(r'^$', views.IndexView.as_view(), name='index'),
 
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
     # /music/<album_id>/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
--- music/views.py
+++ music/views.py
@@ -1,7 +1,11 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
 from django.core.urlresolvers import reverse_lazy
 from .models import Album
+from .forms import UserForm
 
 
 class IndexView(generic.ListView):
@@ -30,3 +34,48 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
     model = Album
     success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+    
+    # display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+    
+    # process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+        
+        if form.is_valid():
+            user = form.save(commit=False)
+            # cleaned (normalized) data
+            # unify data 
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # return user object if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+
+        return render(request, self.template_name, {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
