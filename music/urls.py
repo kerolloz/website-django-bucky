be7ab492334e51acabde872dd4f489add6eafb10
Kerolloz <kerolloz@yahoo.com>
2018-07-29 04:13:05 +0200

    till tut 29

---
+++ music/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from music import views
+
+app_name = 'music'
+
+urlpatterns = [
+    # /music/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+
+    # /music/<album_id>/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+]
